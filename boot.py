d336c1913cf184d6800ed54c6ac1be7b5a98e41d
kubeksy <kubaddd@gmail.com>
2024-09-29 19:14:27 +0200

    initial 3, ota

--- boot.py
+++
@@ -1,5 +0,0 @@
-import ugit
-
-# ugit.backup()  # good idea to backup your files!
-
-ugit.pull_all()
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from machine import Pin
 from utime import sleep
+from WIFI_CONFIG import PASSWORD, SSID
+
+from ota import OTAUpdater
+
+token = "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"
+firmware_url = "https://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@raw.githubusercontent.com/kubu/pico_terminal/master"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+ota_updater.download_and_install_update_if_available()
 
 pin = Pin("LED", Pin.OUT)
 
@@ -7,7 +16,7 @@ print("LED starts flashing...")
 while True:
     try:
         pin.toggle()
-        sleep(1) # sleep 1sec
+        sleep(3)  # sleep 1sec
     except KeyboardInterrupt:
         break
 pin.off()
